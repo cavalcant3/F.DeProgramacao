83e884aed4ac64e05272bbb0a9ac830941b9520c
Matheus Cavalcante <jmatheusoc269@gmail.com>
2021-12-22 06:47:00 -0300

    resolução q1

---
+++ src/com/company/Lista01/Q1.java
@@ -0,0 +1,12 @@
+package com.company.Lista01;
+
+import java.util.Scanner;
+
+public class Q1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float a = sc.nextFloat();
+        float b = a * a;
+        System.out.printf("%.2f", b);
+    }
+}
