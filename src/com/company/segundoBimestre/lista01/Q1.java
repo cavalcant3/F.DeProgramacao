a6ca57ff4b58e58523304e1f2cab86ab452e4de0
Matheus Cavalcante <jmatheusoc269@gmail.com>
2022-01-08 22:34:36 -0300

    comentários

--- src/com/company/segundoBimestre/lista01/Q1.java
+++ src/com/company/segundoBimestre/lista01/Q1.java
@@ -10,7 +10,8 @@ public class Q1 {
         //aqui vair ser um leitor para o usuario
 //        Aqui pegamos o valor inicial
         double ValorI = 15.5;
-        double Desconto = 0.09;
+        double Desconto = 0.09; //poderiamos fazer da seguinte maneira : double Desconto = Valor1 * 0.09;
+                                // e na linha 18 tirariamos o cálculo de lá, ficando um código mais "limpo"
 //          Aqui pegamos o vlaor inicial, multiplicamos pelo valor do desconto
 //          e depois subtraimos do valor original, atribuindo o valor a nova variavel.
         double  NovoValor = ValorI - (ValorI * Desconto);
