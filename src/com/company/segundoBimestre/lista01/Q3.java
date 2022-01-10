fbd12a005950a9da266fe422a039ac94d3f74059
Matheus Cavalcante <jmatheusoc269@gmail.com>
2022-01-10 14:17:49 -0300

    correção

--- src/com/company/segundoBimestre/lista01/Q3.java
+++ src/com/company/segundoBimestre/lista01/Q3.java
@@ -1,23 +1,31 @@
 package com.company.segundoBimestre.lista01;
 
+import java.text.DecimalFormat;
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
 import java.util.Scanner;
 
 public class Q3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Informe a distancia inicial");
-        double D1 = sc.nextDouble();
-        System.out.println("Informe a distancia final");
-        double D2 = sc.nextDouble();
-
-        System.out.println("Informe a velocidade inicial");
+        System.out.println("Informe a distancia");
+        double DM = sc.nextDouble();
+        System.out.println("Informe a velocidade media");
         double VM = sc.nextDouble();
-        double DF = D1 - D2;
-        double TI = DF / VM;
-//        convertendo velocidade para m/s
+        double TM = DM / VM;
+//        converetendo de km para m/s
         VM = VM / 3.6;
-        System.out.println("Velocidade em m/s" + VM);
-        System.out.println("Tempo" + TI);
+//      formatando numeros
+        String PatternNumber = "###,###.##";
+        DecimalFormat df = new DecimalFormat(PatternNumber);
+        Format time = new SimpleDateFormat("HH:mm:ss");
+
+
+        System.out.println("A velocidade em metros/segundos é: " + df.format(VM) + "M/S");
+        System.out.println("O tempo médio é: " + time.format(TM));
+
+
 
     }
 }
