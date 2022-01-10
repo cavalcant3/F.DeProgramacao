0769b86c61713346507a1735d4d2f952b44e8e17
Matheus Cavalcante <jmatheusoc269@gmail.com>
2022-01-10 13:48:49 -0300

    tentativa 1

--- src/com/company/segundoBimestre/lista01/Q3.java
+++ src/com/company/segundoBimestre/lista01/Q3.java
@@ -1,5 +1,23 @@
 package com.company.segundoBimestre.lista01;
 
+import java.util.Scanner;
+
 public class Q3 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Informe a distancia inicial");
+        double D1 = sc.nextDouble();
+        System.out.println("Informe a distancia final");
+        double D2 = sc.nextDouble();
+
+        System.out.println("Informe a velocidade inicial");
+        double VM = sc.nextDouble();
+        double DF = D1 - D2;
+        double TI = DF / VM;
+//        convertendo velocidade para m/s
+        VM = VM / 3.6;
+        System.out.println("Velocidade em m/s" + VM);
+        System.out.println("Tempo" + TI);
+
+    }
 }
