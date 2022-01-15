7909f2f390f573721a2f93aa09dce523a2be6742
Matheus Cavalcante <jmatheusoc269@gmail.com>
2022-01-15 16:29:30 -0300

    adicionando comentários

--- src/com/company/segundoBimestre/lista02/Q1.java
+++ src/com/company/segundoBimestre/lista02/Q1.java
@@ -16,13 +16,13 @@ public class Q1 {
         if (naoNegativo){
             numero = numero * -1; //transformando em positivo para trabalharmos
         }
-        int reverse = 0;
+        int reverse = 0; // o java solicita inicilaizar essa vriável
         int ultimoDigito;
         while (numero >= 1){
-            ultimoDigito = numero % 10;
-            reverse = reverse *10 + ultimoDigito;
-            numero = numero / 10;
+            ultimoDigito = numero % 10; //se o  numero for maior ou igual a 1 nós calculamos o resto e atribuimos a variavel
+            reverse = reverse *10 + ultimoDigito; //e depois multiplicamos para que ela volte ao valor normal
+            numero = numero / 10; //como estamos replicando os valores devemos decrementar do total então dividimos por 10
         }
-        return naoNegativo == true? reverse *-1 : reverse;
+        return naoNegativo == true? reverse *-1 : reverse; //se caso o número em que estivermos trabalhando for negativo ele multiplica por -1, se for positivo retorna normal
     }
 }
