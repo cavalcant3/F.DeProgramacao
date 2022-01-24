b0d30374c146fd93c33951a73f62096a16e86fef
Matheus Cavalcante <jmatheusoc269@gmail.com>
2022-01-24 17:22:04 -0300

    resolução

---
+++ src/com/company/segundoBimestre/lista03/Q4Verificacao.java
@@ -0,0 +1,20 @@
+package com.company.segundoBimestre.lista03;
+
+public class Q4Verificacao {
+    //        criando um array para verificação de estados
+//        percorrendo o array e retornando se está correto ou não
+//    (MG 7%; SP 12%; RJ 15%; MS
+    String[] estadosCorretos  = new String[]{"MG", "SP", "RJ", "MS"};
+
+    public boolean verificacao (String estados){
+        for (int i = 0; i < estadosCorretos.length; i++) {
+            if (estados.equals(estadosCorretos[i])){
+                return true;
+            }
+
+        }
+        System.out.println("ERRO: estado incorreto");;
+        return false;
+    }
+
+}
