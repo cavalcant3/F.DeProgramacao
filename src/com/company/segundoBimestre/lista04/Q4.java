19663f8f914d3eea26574be58cf40f0e4ad7120d
Matheus Cavalcante <jmatheusoc269@gmail.com>
2022-02-08 23:21:25 -0300

    em resolução

---
+++ src/com/company/segundoBimestre/lista04/Q4.java
@@ -0,0 +1,24 @@
+package com.company.segundoBimestre.lista04;
+
+import java.util.Scanner;
+
+/* apresente, ao final, a matrícula do aluno com menor CRE e o CRE médio de
+todos os candidatos. Como entrada, então haverá um String (matrícula) e um float (CRE) para cada
+candidato. A entrada de dados deve parar quando for informada a matrícula 999*/
+public class Q4 {
+    public static void main(String[] args) {
+        String matricula = null;
+        float CRE = 0;
+
+        System.out.println("informe a matricula");
+
+        if (!matricula.equals("999")) {
+            System.out.println("informe a matricula");
+            while (!matricula.equals("999")) {
+                System.out.println("informe a matricula");
+
+            }
+        }
+
+    }
+}
