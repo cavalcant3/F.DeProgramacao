9d2e0b6d4b3b1d62d3aa75c18ee6a5f9a5baf13d
Matheus Cavalcante <jmatheusoc269@gmail.com>
2022-02-15 17:34:15 -0300

    resolução

--- src/com/company/segundoBimestre/lista04/Q4.java
+++ src/com/company/segundoBimestre/lista04/Q4.java
@@ -2,23 +2,48 @@ package com.company.segundoBimestre.lista04;
 
 import java.util.Scanner;
 
-/* apresente, ao final, a matrícula do aluno com menor CRE e o CRE médio de
+
+public class Q4 {
+    /*Fernanda tem um projeto de extensão e precisa selecionar alunos. Escreva um
+programa para que ela possa informar matrícula e CRE (Coeficiente de Rendimento do Estudante)
+dos vários inscritos, e apresente, ao final, a matrícula do aluno com menor CRE e o CRE médio de
 todos os candidatos. Como entrada, então haverá um String (matrícula) e um float (CRE) para cada
 candidato. A entrada de dados deve parar quando for informada a matrícula 999*/
-public class Q4 {
     public static void main(String[] args) {
-        String matricula = null;
-        float CRE = 0;
 
-        System.out.println("informe a matricula");
+        Scanner sc = new Scanner(System.in);
+        String matricula, matricularMenor = null;
+        float cre, menor = 0, creMedio, soma = 0;
+        int cont = 0;
 
-        if (!matricula.equals("999")) {
-            System.out.println("informe a matricula");
-            while (!matricula.equals("999")) {
-                System.out.println("informe a matricula");
-
+        System.out.println("Digite a matricula");
+        matricula = sc.next();
+        while (!matricula.equals("999")) {
+            System.out.println("Digite o cre: ");
+            cre = sc.nextFloat();
+            cont++;
+            if (cont == 1) {
+                menor = cre;
+                matricularMenor = matricula;
+            } else {
+                if (cre <= menor) {
+                    menor = cre;
+                    matricularMenor = matricula;
+                }
             }
+            soma+=cre;
+            System.out.println("Digite a matricula: ");
+            matricula = sc.next();
         }
+        if (cont>0){
+            creMedio= soma/cont;
+            System.out.println("cre medio" +creMedio);
+            System.out.println("Matricula do aluno com menor cre"+ matricularMenor);
+        }else{
+            System.out.println("Nenhum inscrito");
+        }
+
+
 
     }
 }
