ce96a27d12f510d7d46b83168bd3186bca0dd3b7
Matheus Cavalcante <jmatheusoc269@gmail.com>
2022-02-17 20:53:50 -0300

    resolucao

--- src/com/company/segundoBimestre/lista05/q2.java
+++ src/com/company/segundoBimestre/lista05/q2.java
@@ -1,5 +1,6 @@
 package com.company.segundoBimestre.lista05;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class q2 {
@@ -13,17 +14,18 @@ public class q2 {
 
         do {
             System.out.println("informe seu peso");
-            peso = sc.nextDouble();
+            DecimalFormat df = new DecimalFormat("#.##");
+            peso = sc.nextDouble();df.format(peso);
            // total = (peso+tot);
             if (peso<=2){
                 total += peso;
+            cont++;
             } else{
                 System.out.println("limite excedido");
                 break;
             }
-            cont++;
 
-        }while (total!=2);
+        }while (total<2);
 
         System.out.println("limite excedido");
         System.out.println("transporrtando um total de: "+cont+ " pessoas");
