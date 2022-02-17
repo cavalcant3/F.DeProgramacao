40d842127c8e7aa3add8fd5a355db4ef46f58830
Matheus Cavalcante <jmatheusoc269@gmail.com>
2022-02-17 20:42:37 -0300

    resolucao

---
+++ src/com/company/segundoBimestre/lista05/q2.java
@@ -0,0 +1,32 @@
+package com.company.segundoBimestre.lista05;
+
+import java.util.Scanner;
+
+public class q2 {
+    public static void main(String[] args) {
+/*. Escreva um algoritmo que simule a situação. Ou seja, deverá ler o peso de cada pessoa e emitir
+ uma mensagem quando o peso for excedido. Informe ainda quantas pessoas estão sendo transportadas.*/
+        Scanner sc = new Scanner(System.in);
+        double peso;
+        double total = 0;
+        int cont = 0;
+
+        do {
+            System.out.println("informe seu peso");
+            peso = sc.nextDouble();
+           // total = (peso+tot);
+            if (peso<=2){
+                total += peso;
+            } else{
+                System.out.println("limite excedido");
+                break;
+            }
+            cont++;
+
+        }while (total!=2);
+
+        System.out.println("limite excedido");
+        System.out.println("transporrtando um total de: "+cont+ " pessoas");
+
+    }
+}
