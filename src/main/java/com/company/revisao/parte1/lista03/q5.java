8f87efa014e4714cf7812f76022cb123db46c097
Matheus Cavalcante <jmatheusoc269@gmail.com>
2022-02-21 18:38:37 -0300

    resolucao

---
+++ src/main/java/com/company/revisao/parte1/lista03/q5.java
@@ -0,0 +1,11 @@
+package main.java.com.company.revisao.parte1.lista03;
+
+public class q5 {
+    public static void main(String[] args) {
+       /*Escreva um programa que calcule o salário semanal de um trabalhador. As entradas
+são o número de horas usadas na semana e o valor da hora. Até 40 h/semana usa-se a
+valorização dada. Acima de 40h e até 60h há um bônus de 50% para essas horas. Acima de 60h
+há um bônus de 100% para essas horas*/
+
+    }
+}
