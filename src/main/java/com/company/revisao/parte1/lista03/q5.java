7743ca357a56975d97cf609faa50113f81531859
Matheus Cavalcante <jmatheusoc269@gmail.com>
2022-02-21 20:28:14 -0300

    resolucao

--- src/main/java/com/company/revisao/parte1/lista03/q5.java
+++ src/main/java/com/company/revisao/parte1/lista03/q5.java
@@ -7,5 +7,19 @@ são o número de horas usadas na semana e o valor da hora. Até 40 h/semana usa
 valorização dada. Acima de 40h e até 60h há um bônus de 50% para essas horas. Acima de 60h
 há um bônus de 100% para essas horas*/
 
+        double horasSemana = 70;
+        double ValorHoraa = 5;
+        double horaTotal = 0;
+
+        if (horasSemana >= 40 && horasSemana <= 60) {
+            horasSemana += horasSemana * 0.5;
+            horaTotal = horasSemana*ValorHoraa;
+            System.out.println("total valor a receber: "+horaTotal);
+        } else {
+            horasSemana += horasSemana * 1.0;
+            horaTotal = horasSemana*ValorHoraa;
+            System.out.println("total valor a receber: "+horaTotal);
+
+        }
     }
 }
