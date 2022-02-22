4ab1905ad6cebfc4186c0c511ab91ab80c441bd7
Matheus Cavalcante <jmatheusoc269@gmail.com>
2022-02-22 11:05:05 -0300

    resolucao

---
+++ src/main/java/com/company/revisao/parte1/lista04/q1.java
@@ -0,0 +1,11 @@
+package main.java.com.company.revisao.parte1.lista04;
+
+public class q1 {
+    public static void main(String[] args) {
+        int soma=0;
+        for (int i = 0; i <= 100; i++) {
+           soma= soma+i;
+        }
+        System.out.println(soma);
+    }
+}
